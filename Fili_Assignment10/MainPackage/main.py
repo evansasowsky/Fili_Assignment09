48a83380ace08936e41dce199508aae4b5c6249e
Evan Sasowsky <110676861+evansasowsky@users.noreply.github.com>
2022-10-26 14:13:08 -0400

    Update main.py

--- Fili_Assignment10/MainPackage/main.py
+++ Fili_Assignment10/MainPackage/main.py
@@ -1,2 +1,7 @@
 #main.py
-print()
+from ClassesPackage.VillainClass import *
+#from ClassesPackage.HeroClass import *
+#from ClassesPackage.SidekickClass import Sidekick
+
+Villain1 = Villain('Lex Luthor','Male','Active','DC')
+print(Villain1)
